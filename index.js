204c595060f893bd8f4adba04ae8ad4104c170f3
Paweł Pisera <ppisera@e-point.pl>
2021-11-23 19:22:23 +0100

    change port

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 
-const webSocketsServerPort = 8000;
+const webSocketsServerPort = 8888;
 const webSocketServer = require('websocket').server;
 const http = require('http');
 
 // Spinning the http server and the websocket server.
 const server = http.createServer();
 server.listen(webSocketsServerPort);
-console.log('listening on port 8000');
+console.log('listening on port ' , webSocketsServerPort);
 
 
 const wsServer = new webSocketServer({
