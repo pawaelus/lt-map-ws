dae4e4a5cd40da010538e8d95799bfdd79657bf9
Paweł Pisera <ppisera@e-point.pl>
2021-11-23 20:45:39 +0100

    restore

--- index.js
+++ index.js
@@ -5,8 +5,7 @@ const http = require('http');
 
 // Spinning the http server and the websocket server.
 const server = http.createServer();
-server.listen(PORT);
-// server.listen(webSocketsServerPort);
+server.listen(webSocketsServerPort);
 console.log('listening on port ' , webSocketsServerPort);
 
 
